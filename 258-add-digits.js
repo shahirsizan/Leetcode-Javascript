c64c603f0134d3c78a81d0d76f5975f228d674d7
shahirsizan <shahir.sizan18@gmail.com>
2024-09-13 00:02:17 +0600

    another problem solved

---
+++ 258-add-digits.js
@@ -0,0 +1,16 @@
+const addDigits = (num) => {
+	while (num >= 10) {
+		let sum = 0;
+
+		while (num > 0) {
+			sum = sum + (num % 10);
+			num = Math.floor(num / 10);
+		}
+
+		num = sum;
+	}
+
+	return num;
+};
+
+console.log(addDigits(911));
